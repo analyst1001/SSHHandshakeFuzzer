16d14605369a2589cc67bea6d55507ff9e055395
LP <singh414@purdue.edu>
2018-05-03 19:18:02 -0400

    Ignore SIGPIPE to prevent closing on send failure

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cstdlib>
+#include <signal.h>
 #include "Fuzzers/SSHHandshakeStepNValidateFuzzer.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    signal(SIGPIPE, SIG_IGN);
     if (argc < 4) {
         cout<<"USAGE:"<<argv[0]<<" <server IP> <service port> <N>"<<endl;
         return 1;
